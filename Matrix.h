199579d1d3644aa12073431ab2035b86018977a2
VenCOH <204713078+VenCOH@users.noreply.github.com>
2025-10-09 21:55:36 +0200

    update matrix structure and add function for random matrix fill

--- Matrix.h
+++ Matrix.h
@@ -6,26 +6,27 @@
 
 #include <cstring>
 
+template <typename Element>
 class Matrix {
   const size_t m_Dimension;
-  const size_t m_Element_size;
-  std::byte *const m_Data;
+  Element *const m_Data;
 
 public:
-  Matrix(const std::size_t dimension, const std::size_t element_size) :
-    m_Dimension(dimension), m_Element_size(element_size),
-    m_Data(static_cast<std::byte*>(safe_alloc(dimension * dimension, element_size))) {
+  Matrix(const size_t dimension, const size_t element_size) :
+    m_Dimension(dimension),
+    m_Data(static_cast<Element *>(safe_alloc(dimension * dimension,
+                                             sizeof(Element)))) {
   }
 
   ~Matrix() {
     safe_free(m_Data);
   }
 
-  const void* operator[] (const size_t row) const {
-    return reinterpret_cast<void*>(m_Data + m_Dimension * row);
+  Element *operator[](const size_t row) const {
+    return m_Data + m_Dimension * row;
   }
 
   void zero_matrix() const {
-    std::memset(m_Data, 0, m_Dimension * m_Dimension * m_Element_size);
+    std::memset(m_Data, 0, m_Dimension * m_Dimension * sizeof(Element));
   }
 };
--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include "Matrix.h"
+
 #include <cstdio>
 #include <cstdint>
 
@@ -9,14 +11,28 @@
 #include "PathFinder.h"
 
 
-void run_solver_timed(const std::string& finder_name, PathFinder& path_finder, const TransitionMatrix& transition_matrix, Paths& paths) {
+void run_solver_timed(const std::string &finder_name, PathFinder &path_finder,
+                      const TransitionMatrix &transition_matrix, Paths &paths) {
   std::cout << "Running path finder " << finder_name << std::endl;
 
-  const std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+  const std::chrono::steady_clock::time_point start_time =
+      std::chrono::steady_clock::now();
   path_finder.find_paths(transition_matrix, paths);
-  const std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+  const std::chrono::steady_clock::time_point end_time =
+      std::chrono::steady_clock::now();
 
-  std::cout << "Execution time: " << std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count() << "[µs]" << std::endl;
+  std::cout << "Execution time: " << std::chrono::duration_cast<
+        std::chrono::microseconds>(end_time - start_time).count() << "[µs]" <<
+      std::endl;
+}
+
+void random_fill_matrix(const Matrix<unsigned> &matrix, const size_t dimension,
+                        const unsigned number_limit) {
+  std::random_device random_generator;
+
+  for (size_t row = 0; row < dimension; row++)
+    for (size_t col = 0; col < dimension; col++)
+      matrix[row][col] = random_generator() % number_limit;
 }
 
 int main() {
